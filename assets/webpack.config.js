e8da59530e906a8b6f964798735384d48bcd840a
Cody Brunner <rockchalkwushock@icloud.com>
2021-06-16 05:44:26 -0500

    updates build to process css files.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -5,6 +5,7 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 // https://github.com/privatenumber/esbuild-loader
 const { ESBuildMinifyPlugin } = require('esbuild-loader')
 const { sync } = require('glob')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env, options) => {
   const devMode = options.mode !== 'production'
@@ -21,12 +22,22 @@ module.exports = (env, options) => {
           loader: 'esbuild-loader',
           test: /\.js$/,
         },
+        {
+          test: /\.[s]?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
       ],
     },
     optimization: {
       minimizer: [
         new ESBuildMinifyPlugin({
           target: 'es2015',
+          css: true,
         }),
       ],
     },
@@ -36,6 +47,7 @@ module.exports = (env, options) => {
       publicPath: '/js/',
     },
     plugins: [
+      new MiniCssExtractPlugin({ filename: '../css/app.css' }),
       new CopyWebpackPlugin({ patterns: [{ from: 'static/', to: '../' }] }),
     ],
   }
