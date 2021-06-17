68938ba23f47580e7c756b92c8fa626564cfd687
Cody Brunner <rockchalkwushock@icloud.com>
2021-06-17 06:09:45 -0500

    adds get_me/0

--- lib/ice_bear/telegram/client.ex
+++ lib/ice_bear/telegram/client.ex
@@ -1,4 +1,5 @@
 defmodule IceBear.Telegram.Client do
+  alias Finch.Response
   @api_base_uri "https://api.telegram.org"
   @pool_size 2
 
@@ -9,4 +10,17 @@ defmodule IceBear.Telegram.Client do
        @api_base_uri => [size: @pool_size]
      }}
   end
+
+  def get_me do
+    :get
+    |> Finch.build("#{@api_base_uri}/bot#{fetch_token()}/getMe", [], nil)
+    |> Finch.request(__MODULE__)
+    |> parse_response()
+  end
+
+  defp fetch_token, do: Application.fetch_env!(:ice_bear, :bot_token)
+
+  defp parse_response({:ok, %Response{body: body}}), do: body |> Jason.decode!()
+
+  defp parse_response({:error, response}), do: response
 end
