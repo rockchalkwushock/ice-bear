3ae06d520d2e4faae55ee8d8ecc13b844a1cc7f3
Cody Brunner <rockchalkwushock@icloud.com>
2021-07-07 06:52:37 -0500

    chore(ice_bear): adds scaffolding for http client.

---
+++ lib/ice_bear/telegram/client.ex
@@ -0,0 +1,11 @@
+defmodule IceBear.Telegram.Client do
+  @moduledoc """
+  IceBear.Telegram.Client
+
+  The HTTP Client/Wrapper for the Telegram Bot API
+  that IceBear makes use of. I have no need for a
+  full-fledged cleint like Nadia, Telegex, etc for
+  this project so this is a very minimal implementation
+  of the aformentioned projects.
+  """
+end
