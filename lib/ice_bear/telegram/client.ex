ae021e4906f0edc619eb2b56588ad307c60e8440
Cody Brunner <rockchalkwushock@icloud.com>
2021-07-07 09:31:49 -0500

    feat(telegram): adds get_me/0

--- lib/ice_bear/telegram/client.ex
+++ lib/ice_bear/telegram/client.ex
@@ -8,9 +8,14 @@ defmodule IceBear.Telegram.Client do
   this project so this is a very minimal implementation
   of the aformentioned projects.
   """
+  alias Finch.Response
   require Logger
 
   @api_base_uri "https://api.telegram.org"
+  @default_headers [
+    {"Accept", "application/json"},
+    {"Content-Type", "application/json"}
+  ]
   @pool_size 2
 
   def child_spec do
@@ -27,4 +32,24 @@ defmodule IceBear.Telegram.Client do
 
     pool
   end
+
+  def get_me do
+    Logger.debug("----> IceBear.Telegram.Client.get_me/0")
+
+    :get
+    |> Finch.build("#{@api_base_uri}/bot#{fetch_token()}/getMe", @default_headers, nil)
+    |> Finch.request(__MODULE__)
+    |> parse_response()
+    |> process_response()
+  end
+
+  defp fetch_token, do: Application.fetch_env!(:ice_bear, :bot_token)
+
+  defp parse_response({:ok, %Response{body: body}}), do: body |> Jason.decode!()
+
+  defp parse_response({:error, response}), do: response
+
+  defp process_response(%{"ok" => true, "result" => result}), do: {:ok, result}
+
+  defp process_response(%Mint.TransportError{reason: reason}), do: reason
 end
